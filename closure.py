1e1b75b31e629be311a6a216cc8c80aacd006bed
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2021-11-16 19:04:58 +0100

    decorators

--- closure.py
+++ closure.py
@@ -25,3 +25,28 @@ adding = to_be_added(10)
 
 print(adding(5))
 print(adding(20))
+
+#creating parent class
+class Car:
+
+    def __init__(self,name,model):
+        self.name=name
+        self.model=model
+
+    # function to print the car name
+    def printname(self):
+        print("The name of car is:",self.name)
+
+    # function to print the car model
+    def printmodel(self):
+        print("The model of the car is:",self.model)
+class VW(Car):
+
+    def __init__(self):
+        # calling and using the initialization function of Car
+        Car.__init__(self, 'Tiguan', 2020)
+
+vw = VW()
+
+print('The name of car is:', vw.name)
+print('The model of the car is:', vw.model)
