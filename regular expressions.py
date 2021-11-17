1d540c51bf92f4461535e6601fb05594a601ff1e
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2021-11-17 22:03:42 +0100

    regex

--- regular expressions.py
+++ regular expressions.py
@@ -13,3 +13,47 @@ print('normal string output:', normal_string)
 print('raw string output:', raw_string)
 
 
+import re
+
+pattern = r"painting"
+string1 = "This is my favorite painting"
+string2 = "painting is a great hobby"
+
+print(re.match(pattern, string1, re.IGNORECASE))
+print(re.match(pattern, string2, re.IGNORECASE))
+
+
+import re
+
+pattern = r"^Python"
+string1 = "Python is great for text manipulation"
+string2 = "Regexes can be handled very fast by Python"
+
+print(re.findall(pattern, string1))
+print(re.findall(pattern, string2))
+
+
+
+import re
+
+pattern = "P..h.n"
+string = "Python is great for text manipulation, a Pythonista said"
+
+print(re.findall(pattern, string))
+
+
+
+import re
+
+pattern = "tion$"
+string = "Python is great for text manipulation"
+
+print(re.findall(pattern, string))
+
+
+import re
+
+pattern = "da?t"
+string = "dt dat data daaaat day"
+
+print(re.findall(pattern, string))
