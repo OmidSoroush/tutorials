05ab06ed5dca698b240c48bd569d67098c042648
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2021-11-18 10:23:58 +0100

    regex

--- regular expressions.py
+++ regular expressions.py
@@ -57,3 +57,67 @@ pattern = "da?t"
 string = "dt dat data daaaat day"
 
 print(re.findall(pattern, string))
+
+
+import re
+
+pattern = "\^back"
+string = "^back to back"
+
+print(re.findall(pattern, string))
+
+
+import re
+
+pattern = "[xyz]"
+string = "eazy"
+
+print(re.findall(pattern, string))
+
+
+import re
+
+pattern = "a|b|c"
+string = "bar"
+
+print(re.findall(pattern, string))
+
+
+
+import re
+
+pattern1 = "b{2}"
+string1 = "rub rubbber"
+
+print(re.findall(pattern1, string1))
+
+pattern2 = "b{2,4}"
+string2 = "rubb rubbbber"
+
+print(re.findall(pattern2, string2))
+
+
+import re
+
+pattern = "(\w*)@company.com"
+string = "employee@company.com"
+
+print(re.findall(pattern, string))
+
+
+
+import re
+
+pattern = "@"
+string = "employee@company.com"
+
+print(re.split(pattern, string))
+
+
+
+import re
+
+pattern = "@"
+string = "employee@company.com"
+
+print(re.split(pattern, string))
