0217241d3d0aea1def09bda04f4e447dba5638f0
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2021-11-29 09:05:26 +0100

    datetime formatting

--- datetime_moduel.py
+++ datetime_moduel.py
@@ -47,3 +47,117 @@ print("minute =", t.minute)
 print("second =", t.second)
 print("microsecond =", t.microsecond)
 
+
+from datetime import datetime
+
+dt1 = datetime(year=2012, month=3, day=22)
+dt2 = datetime(2012, 3, 22, 11, 34, 16, 236744)
+
+print(dt1)
+print(dt2)
+
+from datetime import datetime
+
+current_date_time = datetime.now()
+print(current_date_time)
+
+from datetime import datetime
+
+my_date = datetime.now()
+
+# get ISO format
+print('ISO format =', my_date.isoformat())
+
+# get ISO weekday
+print('ISO weekday =', my_date.isoweekday())
+
+# get Year
+print('Year =', my_date.year)
+
+# get month
+print('Month =', my_date.month)
+
+# get day
+print('Day =', my_date.day)
+
+
+
+from datetime import datetime
+
+my_date = datetime.now()
+
+# convert datetime object to UNIX timestamp
+timestamp = datetime.timestamp(my_date)
+print('timestamp =', timestamp)
+
+# convert UNIX timestamp to datetime object
+dt = datetime.fromtimestamp(timestamp)
+print('datetime =', dt)
+
+
+
+from datetime import timedelta
+
+td = timedelta(microseconds=3333)
+
+
+
+
+from datetime import datetime, date
+
+date1 = date(2020, 8, 13)
+date2 = date(2019, 9, 14)
+date3 = date2 - date1
+print("date3", date3)
+
+date4 = datetime(year = 2016, month = 5, day = 8, hour = 10, minute = 31, second = 44)
+date5 = datetime(year = 2017, month = 4, day = 6, hour = 7, minute = 32, second = 12)
+date6 = date5 - date4
+print("date6 =", date6)
+
+print("type of date3 =", type(date3))
+print("type of date6 =", type(date6))
+
+
+
+from datetime import timedelta
+
+td1 = timedelta(weeks = 2, days = 7, hours = 12)
+td2 = timedelta(days = 222, hours = 13, minutes = 45, seconds = 18)
+td3 = td2 - td1
+
+print("td3 =", td3)
+
+
+from datetime import timedelta
+
+td1 = timedelta(weeks = 2, days = 7, hours = 12)
+
+print("Total seconds =", td1.total_seconds())
+
+
+from datetime import datetime
+
+string = "11 December, 2020"
+
+dt = datetime.strptime(string, "%d %B, %Y")
+print(dt)
+
+
+
+
+from datetime import datetime
+
+# current date and time
+now = datetime.now()
+
+t = now.strftime("%H:%M:%S")
+print("time:", t)
+
+s1 = now.strftime("%m/%d/%Y, %H:%M:%S")
+# mm/dd/YY H:M:S format
+print("s1:", s1)
+
+s2 = now.strftime("%d/%m/%Y, %H:%M:%S")
+# dd/mm/YY H:M:S format
+print("s2:", s2)
