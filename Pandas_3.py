81e303844be11bd590b6373b1057c4b6d0ebf9e5
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2021-12-02 10:37:28 +0100

    pandas apply

---
+++ Pandas_3.py
@@ -0,0 +1,100 @@
+pd.set_option('display.max_colwidth', None)
+
+import pandas as pd
+df = pd.read_csv("~/Downloads/employees.csv")
+
+mean_age = df.age.mean()
+
+df["dist_mean"] = df.age.map(lambda x: x - mean_age)
+
+
+def remean_points(row):
+    return row.upper()
+
+df.apply(lambda x:x.upper())
+
+df.apply(lambda x : x + 10)
+
+
+
+df[["age", "dist_mean"]].head()
+
+df.info()
+
+df.shape
+
+df.first_name.describe()
+
+df = pd.read_csv("~/Downloads/employees.csv", usecols=["first_name", "last_name"])
+
+# set the first row as column names
+df = pd.read_csv("~/Downloads/employees.csv", header=1)
+
+# skip the first 3 rows
+df = pd.read_csv("~/Downloads/employees.csv", skiprows=3)
+
+
+import pandas as pd
+
+df = pd.read_excel("~/Downloads/employees.xlsx")
+
+df.head()
+
+import pandas as pd
+
+df = pd.read_json("~/Downloads/employees.json")
+
+df.head()
+
+df.to_csv("~/Downloads/data.csv")
+df.to_excel("~/Downloads/data.xlsx")
+
+
+
+import pandas as pd
+df = pd.read_csv("~/Downloads/employees.csv")
+
+df = df.iloc[:,[1,2]]
+df["age"].apply(lambda x : x + 10)
+df.apply(lambda x: x.str.upper())
+
+
+data = [(44, 34, 45),
+         (455, 23, 54),
+         (775, 34, 25),
+         (454, 32, 22),
+         (566, 23, 23)]
+
+df = pd.DataFrame(data, columns=list('ABC'))
+
+def double(x):
+    return x * 2
+
+df["A"] = df["A"].apply(double)
+
+df[["A", "B"]] = df[["A", "B"]].apply(double)
+
+df = df.apply(double, axis=0)
+
+# One column, lambda function
+df["A"] = df["A"].apply(lambda x : x * 2)
+
+# Two columns, lambda function
+df[["A", "B"]] = df[["A", "B"]].apply(lambda x : x * 2)
+
+# Entire DataFrame, lambda function
+import numpy as np
+df = df.apply(sum, axis=0)
+
+df["A"].name
+
+
+data = [(44, 34, 45),
+         (455, 23, 54),
+         (775, 34, 25),
+         (454, 32, 22),
+         (566, 23, 23)]
+
+df = pd.DataFrame(data, columns=list('ABC'))
+
+df = df.apply(lambda x: np.mean(x) if x.name in ['A','B'] else x)
