134ad40587ed7e7a75facb8c0d6db134c66d1f2a
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2021-12-09 21:58:45 +0100

    normal_forms

---
+++ data engineering/normal_forms.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({'Name': ['David', 'Glenn', 'Steve'], 'Subjects': [
+    ['English', 'Math'], ['Math'], ['Science', 'English']]})
+
+lens = list(map(len, df['Subjects'].values))
+
+res = pd.DataFrame({'Name': np.repeat(
+    df['Name'], lens), 'Subject': np.concatenate(df['Subjects'].values)})
+
+print(res)
