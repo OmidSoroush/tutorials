630769970698d548d06ef0f9dc57e1c2b4c200ff
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2022-01-04 12:38:26 +0100

    PySpark handling missing data part 2

--- PySpark/missing_data.py
+++ PySpark/missing_data.py
@@ -4,7 +4,7 @@ columns = ["first_name", "last_name", "gender", "language", "age", "salary"]
 
 data = [("Krishna", "Popplestone", "Non-binary", "German", 85, 52000),
         ("Wilbert", "Gavaran", "Male", "Thai", None, 43000),
-        ("Anthe", "Klulisek", None, "Persian", 37, None),
+        (None, None, None, None, 37, None),
         ("Filbert",	None, "Male", "Maltese", 43, 35000),
         ("Lisetta",	"Cohan", "Non-binary", "English", 32, 75000),
         ("Shellie",	None, "Female", "Maltese", None, 44000),
@@ -18,3 +18,18 @@ spark = SparkSession.builder \
 
 df = spark.createDataFrame(data, columns)
 df.show(3)
+
+df.na.drop(subset=["last_name", "salary"]).show(3)
+
+df.na.fill("No Last Name", subset=["last_name"]).show(3)
+
+from pyspark.sql.functions import mean
+
+# calculate the mean
+mean_age = df.select(mean(df["age"])).collect()
+
+# get the actual mean value
+mean_age = mean_age[0][0]
+
+# fill the missings with the mean
+df.na.fill(mean_age, subset=["age"]).show(3)
